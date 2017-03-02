bd18edd8007d4ac4a21fe91c5d86269c755e2c4e
richardmcmullin <richard.e.mcmullin@gmail.com>
2017-03-02 08:37:51 -0500

    Filter the CounterEncoder rate to throw away bad values.

--- TorontoJar/src/com/toronto/sensors/T_CounterEncoder.java
+++ TorontoJar/src/com/toronto/sensors/T_CounterEncoder.java
@@ -6,9 +6,14 @@ public class T_CounterEncoder extends T_Encoder {
 	
 	private final Counter counter;
 
-	public T_CounterEncoder(int channel) {
+	private double prevRate = 0;
+	private int    badDataCount = 0;
+	private double maxValidRate;
+	
+	public T_CounterEncoder(int channel, double maxRate) {
 		counter = new Counter(channel);
 		counter.setDistancePerPulse(1.0);
+		this.maxValidRate = maxRate *1.5;
 	}
 	
 	/**
@@ -20,13 +25,53 @@ public class T_CounterEncoder extends T_Encoder {
 	@Override
 	public int get() {
 		return counter.get();
-
 	}
 
 	@Override
 	public double getRate() {
-		return counter.getRate();
+		
+		// Note: The counter encoder can be very noisy.
+		// Occasionally, the encoder reads very large
+		// numbers, especially in the case where
+		// another nearby motor starts up.
+		double rate = counter.getRate();
+		
+		// Throw away bad data
+		// The value is bad if the rate < 0;
+		if (rate < 0) {
+			return prevRate;
+		}
+		
+		// The value is bad if the rate is 
+		// more than 150% of the maxRate
+		if (rate > maxValidRate) {
+			return prevRate;
+		}
 
+		// The encoder should not slew at more than
+		// 20 rotations per second per 20ms
+		// Note: this number is arbitrary.
+		double delta = rate - prevRate;
+		
+		if ( Math.abs(delta) > 20.0 ) {
+			
+			badDataCount++;
+
+			// If there were 10 bad data elements
+			// in a row, then assume the data is 
+			// good and continue from this point
+			if (badDataCount > 10) {
+				badDataCount = 0;
+				prevRate = rate;
+				return rate;
+			}
+			
+			return prevRate;
+		}
+		
+		badDataCount = 0;
+		prevRate = rate;
+		return rate;
 	}
 
 	@Override
